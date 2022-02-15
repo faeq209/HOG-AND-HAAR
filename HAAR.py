08bae83be7de6e814bcb3a1f22d339c022fea5ae
faeq209 <faeq.hussain.1860@gmail.com>
2022-02-15 21:44:31 +0530

    add

--- HAAR.py
+++ HAAR.py
@@ -7,6 +7,8 @@ camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 45
 camera.rotation = 180
+ntime=0
+ptime=0
 rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 haarcas=cv.CascadeClassifier('body.xml')
@@ -16,10 +18,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     boxes = haarcas.detectMultiScale(image, winStride=(8,8) )
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-    for (xA, yA, xB, yB) in boxes:
+    for (x, y,w,h) in boxes:
         # display the detected boxes in the colour picture
-        cv.rectangle(image, (xA, yA), (xB, yB),(0, 255, 0), 2)
+        cv.rectangle(image, (x,y), (x+w, y+h),(0, 255, 0), 2)
+        cv.putText(image,"person",(x,y-5),cv.FONT_HERSHEY_SIMPLEX,1,(100,255,0),1,cv.LINE_AA)
+    ntime=time.time()
+    fps=1/(ntime-ptime)
+    ptime=ntime
+    fps=(int)(fps)
+    fps=str(fps)
+    cv.putText(image,fps,(7,70),cv.FONT_HERSHEY_SIMPLEX,3,(100,255,0),3,cv.LINE_AA)
     cv.imshow("Frame", image);
     key = cv.waitKey(1) & 0xFF
     rawCapture.truncate(0)
